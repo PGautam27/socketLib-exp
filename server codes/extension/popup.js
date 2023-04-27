b8361817f1cf2f4f3744d0b704209d54ea9e1ee7
P Gautam <92343715+PGautam27@users.noreply.github.com>
2023-04-28 00:42:12 +0530

    Update popup.js

--- server codes/extension/popup.js
+++ server codes/extension/popup.js
@@ -1,11 +1,16 @@
 
+// Here socket is the variable connecting to the websocket server.
 let socket = new WebSocket("ws://localhost:8080")
-        socket.onopen = function(e) {
-            console.log("socket established")
+
+// On Successfull connection establishment it will console socket established.
+// You can customise to your requirement
+socket.onopen = function(e) {
+  console.log("socket established")
 };
 
+// This is just a button logic which isn't required
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("myButton").addEventListener("click", function() {
+  document.getElementById("myButton").addEventListener("click", function() {
       alert("Hello, world!");
-    });
   });
+});
